2a01882414acb8fc3c4ffcdd99e7008de3ba1e5d
Serban Teodorescu <teodorescu.serban@gmail.com>
2022-07-22 10:52:42 +0300

    [OPS-8463] read the json file content, dont json parse the filename

--- src/countries.js
+++ src/countries.js
@@ -403,7 +403,7 @@ function CountryManager(config, logger, trelloClient, rwapiClient, date) {
 /**
  * Execute logic.
  */
-const config = JSON.parse(process.env.CONFIG);
+const config = require(process.env.CONFIG);
 
 const Logger = require('./libs/logger.js').Logger;
 const logger = new Logger(config.debug);
